fb7a20b97936a27c7d710ea78dfcc8478c8ab580
Jezell <jzlldomer@gmail.com>
2021-11-13 23:06:33 +0800

    94-100 percentage

---
+++ PUP_grading.py
@@ -0,0 +1,6 @@
+
+percent = int(input("Grade Percentage: "))
+if percent >= 97 and percent <= 100:
+    print ("Grade/Mark is 1.0. Excellent!")
+elif percent >= 94 and percent <= 96:
+    print ("Grade/Mark is 1.25. Excellent!")
