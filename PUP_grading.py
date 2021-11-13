3eb48c000f033dd35a369831237a89dc037d7a0d
Jezell <jzlldomer@gmail.com>
2021-11-13 23:18:20 +0800

    65-93 percentage

--- PUP_grading.py
+++ PUP_grading.py
@@ -4,3 +4,19 @@ if percent >= 97 and percent <= 100:
     print ("Grade/Mark is 1.0. Excellent!")
 elif percent >= 94 and percent <= 96:
     print ("Grade/Mark is 1.25. Excellent!")
+elif percent >= 91 and percent <= 93:
+    print ("Grade/Mark is 1.5. Very Good")
+elif percent >= 88 and percent <= 90:
+    print ("Grade/Mark is 1.75. Very Good")
+elif percent >= 85 and percent <= 87:
+    print ("Grade/Mark is 2.0. Good")
+elif percent >= 82 and percent <= 84:
+    print ("Grade/Mark is 2.25. Good")
+elif percent >= 79 and percent <= 81
+    print ("Grade/Mark is 2.5. Satisfactory")
+elif percent >= 76 and percent <= 78:
+    print ("Grade/Mark is 2.75 is Satisfactory")
+elif percent == 75:
+    print ("Grade/Mark is 3.0 is Passing")
+elif percent >= 65 and percent <= 74:
+    print ("Grade/Mark is 5.0 is Failure")
