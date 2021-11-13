a358ed3737ed1a3c541507c2ac92ecbcc0b449af
Jezell <jzlldomer@gmail.com>
2021-11-13 22:49:55 +0800

    functions for smallest number

--- lowest_number.py
+++ lowest_number.py
@@ -1,7 +1,7 @@
 
 number1 = int(input("Enter First number: "))
-number2 = int(input("Enter First number: "))
-number3 = int(input("Enter First number: "))
+number2 = int(input("Enter Second number: "))
+number3 = int(input("Enter Third number: "))
 
 def largest(num1, num2, num3):
     if (num1 > num2) and (num1 > num3):
@@ -12,6 +12,16 @@ def largest(num1, num2, num3):
         largest_num = num3
     print("The largest number between the 3 is: ", largest_num)
 
+def smallest(num1, num2, num3):
+    if (num1 < num2) and (num1 < num3):
+        smallest_num = num1
+    elif (num2 < num1) and (num2 < num3):
+        smallest_num = num2
+    else:
+        smallest_num = num3
+    print("The smallest number between the 3 is: ", smallest_num)
+
 largest(number1, number2, number3)
+smallest(number1, number2, number3)
 
 
