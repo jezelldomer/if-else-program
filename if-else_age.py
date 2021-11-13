e8d03a80dd6f4975a5794b7a2f96760aace9675a
Jezell <jzlldomer@gmail.com>
2021-11-14 00:09:28 +0800

    else for negative input

--- if-else_age.py
+++ if-else_age.py
@@ -2,3 +2,13 @@
 your_age = int(input("Enter your age: "))
 if your_age >= 0 and your_age <= 12: 
     print ("Kid")
+elif your_age >= 13 and your_age <= 17:
+    print ("Teen")
+elif your_age == 18:
+    print ("Debut")
+elif your_age >= 19:
+    print ("Adult")
+else:
+    print("Invalid input for your age. Please provide and enter a valid input. ")
+
+print ("--Done--")
