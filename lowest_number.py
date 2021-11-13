4a4b64372c54ec5a0d7769384f19d512134ba7d6
Jezell <jzlldomer@gmail.com>
2021-11-13 22:26:07 +0800

    inputs and function

---
+++ lowest_number.py
@@ -0,0 +1,10 @@
+
+number1 = int(input("Enter First number: "))
+number2 = int(input("Enter First number: "))
+number3 = int(input("Enter First number: "))
+
+def largest(num1, num2, num3):
+    if (num1 > num2) and (num1 > num3):
+        largest_num = num1
+
+
