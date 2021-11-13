4e8a6936d085675f45d2ab2930a1e908ed9c012b
Jezell <jzlldomer@gmail.com>
2021-11-13 22:40:51 +0800

    functions for largest number

--- lowest_number.py
+++ lowest_number.py
@@ -6,5 +6,12 @@ number3 = int(input("Enter First number: "))
 def largest(num1, num2, num3):
     if (num1 > num2) and (num1 > num3):
         largest_num = num1
+    elif (num2 > num1) and (num2 > num3):
+        largest_num = num2
+    else:
+        largest_num = num3
+    print("The largest number between the 3 is: ", largest_num)
+
+largest(number1, number2, number3)
 
 
